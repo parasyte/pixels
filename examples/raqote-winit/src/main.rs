6c36b3955be558975379bbeb86cc2477da4fa1ac
Jay Oster <jay@kodewerx.org>
2020-09-18 03:59:05 -0700

    Add an example with raqote (#118)
    * Add an example with raqote
    
    * Fix raqote dependency

---
+++ examples/raqote-winit/src/main.rs
@@ -0,0 +1,89 @@
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+
+use crate::shapes::Shapes;
+use log::error;
+use pixels::{Error, Pixels, SurfaceTexture};
+use std::time::Instant;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+mod shapes;
+
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 400;
+
+fn main() -> Result<(), Error> {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Hello Raqote")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let (mut pixels, mut shapes) = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+
+        let pixels = Pixels::new(WIDTH, HEIGHT, surface_texture)?;
+        let shapes = Shapes::new(window_size.width, window_size.height);
+
+        (pixels, shapes)
+    };
+
+    let mut now = Instant::now();
+
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        if let Event::RedrawRequested(_) = event {
+            for (dst, &src) in pixels
+                .get_frame()
+                .chunks_exact_mut(4)
+                .zip(shapes.get_frame().iter())
+            {
+                dst[0] = (src >> 16) as u8;
+                dst[1] = (src >> 8) as u8;
+                dst[2] = src as u8;
+                dst[3] = (src >> 24) as u8;
+            }
+
+            if pixels
+                .render()
+                .map_err(|e| error!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize(size.width, size.height);
+            }
+
+            // Update internal state and request a redraw
+            shapes.draw(now.elapsed().as_secs_f32());
+            window.request_redraw();
+
+            now = Instant::now();
+        }
+    });
+}
