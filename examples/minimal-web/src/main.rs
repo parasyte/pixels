1c1791ea2c249e833c4b959d0f0e8dd0288cf958
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-10-31 23:50:36 -0500

    Implement resize functionality for minimal-web example

--- examples/minimal-web/src/main.rs
+++ examples/minimal-web/src/main.rs
@@ -3,6 +3,7 @@
 
 use log::error;
 use pixels::{PixelsBuilder, SurfaceTexture};
+use std::rc::Rc;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
@@ -50,10 +51,30 @@ async fn run() {
             .expect("WindowBuilder error")
     };
 
+    let window = Rc::new(window);
+
     #[cfg(target_arch = "wasm32")]
     {
+        use wasm_bindgen::JsCast;
         use winit::platform::web::WindowExtWebSys;
 
+        // Retrieve current width and height dimensions of browser client window
+        let get_window_size = || {
+            let client_window = web_sys::window().unwrap();
+            LogicalSize::new(
+                client_window.inner_width().unwrap().as_f64().unwrap(),
+                client_window.inner_height().unwrap().as_f64().unwrap()
+            )
+        };
+
+        let window = Rc::clone(&window);
+
+        // Initialize winit window with current dimensions of browser client
+        window.set_inner_size(get_window_size());
+
+        let client_window = web_sys::window().unwrap();
+
+        // Attach winit canvas to body element 
         web_sys::window()
             .and_then(|win| win.document())
             .and_then(|doc| doc.body())
@@ -62,12 +83,23 @@ async fn run() {
                     .ok()
             })
             .expect("couldn't append canvas to document body");
+        
+        // Listen for resize event on browser client. Adjust winit window dimensions
+        // on event trigger
+        let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
+            let size = get_window_size();
+            window.set_inner_size(size)
+        }) as Box<dyn FnMut(_)>);
+        client_window
+            .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+            .unwrap();
+        closure.forget();
     }
 
     let mut input = WinitInputHelper::new();
     let mut pixels = {
         let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, window.as_ref());
         PixelsBuilder::new(WIDTH, HEIGHT, surface_texture)
             .wgpu_backend(pixels::wgpu::Backends::all())
             .device_descriptor(pixels::wgpu::DeviceDescriptor {
