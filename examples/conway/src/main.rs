a3b6a4bd05a4d1c3200a1096a5e3aa2b86f440cc
Jay Oster <jay@kodewerx.org>
2022-01-08 03:22:19 -0800

    Fix crash on Wayland
    - Rather than setting the position and window size based on available
      screen space, this PR just creates a window with a default size and
      doesn't attempt to set the position.
    - Closes #29

--- examples/conway/src/main.rs
+++ examples/conway/src/main.rs
@@ -3,25 +3,40 @@
 
 use log::{debug, error};
 use pixels::{Error, Pixels, SurfaceTexture};
-use winit::dpi::{LogicalPosition, LogicalSize, PhysicalSize};
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 use winit_input_helper::WinitInputHelper;
 
-const SCREEN_WIDTH: u32 = 400;
-const SCREEN_HEIGHT: u32 = 300;
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 300;
 
 fn main() -> Result<(), Error> {
     env_logger::init();
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
-    let (window, p_width, p_height, mut _hidpi_factor) =
-        create_window("Conway's Game of Life", &event_loop);
 
-    let surface_texture = SurfaceTexture::new(p_width, p_height, &window);
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let scaled_size = LogicalSize::new(WIDTH as f64 * 3.0, HEIGHT as f64 * 3.0);
+        WindowBuilder::new()
+            .with_title("Conway's Game of Life")
+            .with_inner_size(scaled_size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
 
-    let mut life = ConwayGrid::new_random(SCREEN_WIDTH as usize, SCREEN_HEIGHT as usize);
-    let mut pixels = Pixels::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture)?;
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    let mut life = ConwayGrid::new_random(WIDTH as usize, HEIGHT as usize);
     let mut paused = false;
 
     let mut draw_state: Option<bool> = None;
@@ -108,10 +123,6 @@ fn main() -> Result<(), Error> {
                     draw_state = None;
                 }
             }
-            // Adjust high DPI factor
-            if let Some(factor) = input.scale_factor_changed() {
-                _hidpi_factor = factor;
-            }
             // Resize the window
             if let Some(size) = input.window_resized() {
                 pixels.resize_surface(size.width, size.height);
@@ -122,64 +133,6 @@ fn main() -> Result<(), Error> {
             window.request_redraw();
         }
     });
-}
-
-// COPYPASTE: ideally this could be shared.
-
-/// Create a window for the game.
-///
-/// Automatically scales the window to cover about 2/3 of the monitor height.
-///
-/// # Returns
-///
-/// Tuple of `(window, surface, width, height, hidpi_factor)`
-/// `width` and `height` are in `PhysicalSize` units.
-fn create_window(
-    title: &str,
-    event_loop: &EventLoop<()>,
-) -> (winit::window::Window, u32, u32, f64) {
-    // Create a hidden window so we can estimate a good default window size
-    let window = winit::window::WindowBuilder::new()
-        .with_visible(false)
-        .with_title(title)
-        .build(event_loop)
-        .unwrap();
-    let hidpi_factor = window.scale_factor();
-
-    // Get dimensions
-    let width = SCREEN_WIDTH as f64;
-    let height = SCREEN_HEIGHT as f64;
-    let (monitor_width, monitor_height) = {
-        if let Some(monitor) = window.current_monitor() {
-            let size = monitor.size().to_logical(hidpi_factor);
-            (size.width, size.height)
-        } else {
-            (width, height)
-        }
-    };
-    let scale = (monitor_height / height * 2.0 / 3.0).round().max(1.0);
-
-    // Resize, center, and display the window
-    let min_size: winit::dpi::LogicalSize<f64> =
-        PhysicalSize::new(width, height).to_logical(hidpi_factor);
-    let default_size = LogicalSize::new(width * scale, height * scale);
-    let center = LogicalPosition::new(
-        (monitor_width - width * scale) / 2.0,
-        (monitor_height - height * scale) / 2.0,
-    );
-    window.set_inner_size(default_size);
-    window.set_min_inner_size(Some(min_size));
-    window.set_outer_position(center);
-    window.set_visible(true);
-
-    let size = default_size.to_physical::<f64>(hidpi_factor);
-
-    (
-        window,
-        size.width.round() as u32,
-        size.height.round() as u32,
-        hidpi_factor,
-    )
 }
 
 /// Generate a pseudorandom seed for the game's PRNG.
