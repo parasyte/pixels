6ca5d0063320bab031fd7930d6806996e97b91b3
Jay Oster <jay@kodewerx.org>
2022-10-02 20:33:12 -0700

    Update run-wasm to latest development revision

--- run-wasm/src/main.rs
+++ run-wasm/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    cargo_run_wasm::run_wasm();
+    let css = r#"body {
+        background-color: #000;
+        margin: 0;
+        overflow: hidden;
+    }"#;
+
+    cargo_run_wasm::run_wasm_with_css(css);
 }
