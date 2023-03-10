899e27bafa6eac42ced144878f5c946e49139536
Jay Oster <jay@kodewerx.org>
2023-03-09 19:42:57 -0800

    Remove get_ and set_ prefixes from public APIs (#341)
    This renames:
    
    - `get_frame` to `frame`
    - `get_frame_mut` to `frame_mut`
    - `set_clear_color` to `clear_color`
    
    Which more closely follows convention set by the standard library et al.

--- src/lib.rs
+++ src/lib.rs
@@ -269,10 +269,10 @@ impl Pixels {
     /// let mut pixels = Pixels::new(320, 240, surface_texture)?;
     ///
     /// // Set clear color to red.
-    /// pixels.set_clear_color(Color::RED);
+    /// pixels.clear_color(Color::RED);
     /// # Ok::<(), pixels::Error>(())
     /// ```
-    pub fn set_clear_color(&mut self, color: wgpu::Color) {
+    pub fn clear_color(&mut self, color: wgpu::Color) {
         self.context.scaling_renderer.clear_color = color;
     }
 
@@ -398,7 +398,7 @@ impl Pixels {
     /// let mut pixels = Pixels::new(320, 240, surface_texture)?;
     ///
     /// // Clear the pixel buffer
-    /// let frame = pixels.get_frame_mut();
+    /// let frame = pixels.frame_mut();
     /// for pixel in frame.chunks_exact_mut(4) {
     ///     pixel[0] = 0x00; // R
     ///     pixel[1] = 0x00; // G
@@ -443,7 +443,7 @@ impl Pixels {
     /// let mut pixels = Pixels::new(320, 240, surface_texture)?;
     ///
     /// // Clear the pixel buffer
-    /// let frame = pixels.get_frame_mut();
+    /// let frame = pixels.frame_mut();
     /// for pixel in frame.chunks_exact_mut(4) {
     ///     pixel[0] = 0x00; // R
     ///     pixel[1] = 0x00; // G
@@ -538,7 +538,7 @@ impl Pixels {
 
     /// Get a mutable byte slice for the pixel buffer. The buffer is _not_ cleared for you; it will
     /// retain the previous frame's contents until you clear it yourself.
-    pub fn get_frame_mut(&mut self) -> &mut [u8] {
+    pub fn frame_mut(&mut self) -> &mut [u8] {
         &mut self.pixels
     }
 
@@ -546,7 +546,7 @@ impl Pixels {
     ///
     /// This may be useful for operations that must sample the buffer, such as blending pixel
     /// colours directly into it.
-    pub fn get_frame(&self) -> &[u8] {
+    pub fn frame(&self) -> &[u8] {
         &self.pixels
     }
 
