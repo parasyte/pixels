b82acfc7e597f6684672e9fb9fa7b3de001991be
Michael Krasnitski <michael.krasnitski@gmail.com>
2024-01-29 14:50:46 -0500

    Fix minimal-tao example
    Removed menu as it's no longer supported directly by `tao`.

--- examples/minimal-tao/src/main.rs
+++ examples/minimal-tao/src/main.rs
@@ -4,11 +4,11 @@
 use error_iter::ErrorIter as _;
 use log::error;
 use pixels::{Error, Pixels, SurfaceTexture};
+use std::sync::Arc;
 use tao::dpi::LogicalSize;
 use tao::event::{Event, KeyEvent, WindowEvent};
 use tao::event_loop::{ControlFlow, EventLoop};
 use tao::keyboard::KeyCode;
-use tao::menu::{MenuBar, MenuItem};
 use tao::window::WindowBuilder;
 
 const WIDTH: u32 = 320;
@@ -27,25 +27,20 @@ fn main() -> Result<(), Error> {
     env_logger::init();
     let event_loop = EventLoop::new();
     let window = {
-        let mut file_menu = MenuBar::new();
-        file_menu.add_native_item(MenuItem::Quit);
-
-        let mut menu = MenuBar::new();
-        menu.add_submenu("File", true, file_menu);
-
         let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        WindowBuilder::new()
+        let window = WindowBuilder::new()
             .with_title("Hello Pixels/Tao")
-            .with_menu(menu)
             .with_inner_size(size)
             .with_min_inner_size(size)
             .build(&event_loop)
-            .unwrap()
+            .unwrap();
+        Arc::new(window)
     };
 
     let mut pixels = {
         let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        let surface_texture =
+            SurfaceTexture::new(window_size.width, window_size.height, Arc::clone(&window));
         Pixels::new(WIDTH, HEIGHT, surface_texture)?
     };
     let mut world = World::new();
