26f7a4045eef71f6cd93ea4d9f8f5026480d5b21
Michael Krasnitski <michael.krasnitski@gmail.com>
2024-01-29 11:32:19 -0500

    Bump `wgpu` to 0.19 and `winit` to 0.29

--- internals/pixels-mocks/src/lib.rs
+++ internals/pixels-mocks/src/lib.rs
@@ -1,57 +1,19 @@
 #![deny(clippy::all)]
 
-pub struct Rwh;
+pub struct Window;
 
-unsafe impl raw_window_handle::HasRawWindowHandle for Rwh {
-    fn raw_window_handle(&self) -> raw_window_handle::RawWindowHandle {
-        #[cfg(target_os = "macos")]
-        return raw_window_handle::RawWindowHandle::AppKit(
-            raw_window_handle::AppKitWindowHandle::empty(),
-        );
-        #[cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd",
-        ))]
-        return raw_window_handle::RawWindowHandle::Wayland(
-            raw_window_handle::WaylandWindowHandle::empty(),
-        );
-        #[cfg(target_os = "windows")]
-        return raw_window_handle::RawWindowHandle::Win32(
-            raw_window_handle::Win32WindowHandle::empty(),
-        );
-        #[cfg(target_os = "ios")]
-        return raw_window_handle::RawWindowHandle::UiKit(
-            raw_window_handle::UiKitWindowHandle::empty(),
-        );
+impl raw_window_handle::HasWindowHandle for Window {
+    fn window_handle(
+        &self,
+    ) -> Result<raw_window_handle::WindowHandle<'_>, raw_window_handle::HandleError> {
+        unimplemented!()
     }
 }
 
-unsafe impl raw_window_handle::HasRawDisplayHandle for Rwh {
-    fn raw_display_handle(&self) -> raw_window_handle::RawDisplayHandle {
-        #[cfg(target_os = "macos")]
-        return raw_window_handle::RawDisplayHandle::AppKit(
-            raw_window_handle::AppKitDisplayHandle::empty(),
-        );
-        #[cfg(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "openbsd",
-        ))]
-        return raw_window_handle::RawDisplayHandle::Wayland(
-            raw_window_handle::WaylandDisplayHandle::empty(),
-        );
-        #[cfg(target_os = "windows")]
-        return raw_window_handle::RawDisplayHandle::Windows(
-            raw_window_handle::WindowsDisplayHandle::empty(),
-        );
-        #[cfg(target_os = "ios")]
-        return raw_window_handle::RawDisplayHandle::UiKit(
-            raw_window_handle::UiKitDisplayHandle::empty(),
-        );
+impl raw_window_handle::HasDisplayHandle for Window {
+    fn display_handle(
+        &self,
+    ) -> Result<raw_window_handle::DisplayHandle<'_>, raw_window_handle::HandleError> {
+        unimplemented!()
     }
 }
