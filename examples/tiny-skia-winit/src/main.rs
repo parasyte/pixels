d79d44924b42925ec56644b3beba626952705918
Nico Burns <nico@nicoburns.com>
2026-01-19 17:33:46 +0000

    Fix tiny-skia example
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- examples/tiny-skia-winit/src/main.rs
+++ examples/tiny-skia-winit/src/main.rs
@@ -4,13 +4,14 @@
 use error_iter::ErrorIter as _;
 use log::error;
 use pixels::{Error, Pixels, SurfaceTexture};
+use std::sync::Arc;
 use std::time::Instant;
 use tiny_skia::Pixmap;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::keyboard::KeyCode;
-use winit::window::WindowBuilder;
+use winit::window::Window;
 use winit_input_helper::WinitInputHelper;
 
 mod shape;
@@ -24,12 +25,17 @@ fn main() -> Result<(), Error> {
     let mut input = WinitInputHelper::new();
     let window = {
         let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        WindowBuilder::new()
-            .with_title("Hello tiny-skia")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
+        #[allow(deprecated)]
+        Arc::new(
+            event_loop
+                .create_window(
+                    Window::default_attributes()
+                        .with_title("Hello tiny-skia")
+                        .with_inner_size(size)
+                        .with_min_inner_size(size),
+                )
+                .unwrap(),
+        )
     };
 
     let mut pixels = {
@@ -42,41 +48,49 @@ fn main() -> Result<(), Error> {
     let mut drawing = Pixmap::new(WIDTH, HEIGHT).unwrap();
     let now = Instant::now();
 
+    #[allow(deprecated)]
     let res = event_loop.run(|event, elwt| {
-        // Draw the current frame
-        if let Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            ..
-        } = event
-        {
-            pixels.frame_mut().copy_from_slice(drawing.data());
-            if let Err(err) = pixels.render() {
-                log_error("pixels.render", err);
-                elwt.exit();
-                return;
+        match event {
+            Event::Resumed => {}
+            Event::NewEvents(_) => input.step(),
+            Event::AboutToWait => input.end_step(),
+            Event::DeviceEvent { event, .. } => {
+                input.process_device_event(&event);
             }
-        }
+            Event::WindowEvent { event, .. } => {
+                // Draw the current frame
+                if event == WindowEvent::RedrawRequested {
+                    pixels.frame_mut().copy_from_slice(drawing.data());
+                    if let Err(err) = pixels.render() {
+                        log_error("pixels.render", err);
+                        elwt.exit();
+                        return;
+                    }
+                }
 
-        // Handle input events
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
-                elwt.exit();
-                return;
-            }
+                // Handle input events
+                if input.process_window_event(&event) {
+                    // Close events
+                    if input.key_pressed(KeyCode::Escape) || input.close_requested() {
+                        elwt.exit();
+                        return;
+                    }
 
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    log_error("pixels.resize_surface", err);
-                    elwt.exit();
-                    return;
+                    // Resize the window
+                    if let Some(size) = input.window_resized() {
+                        if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                            log_error("pixels.resize_surface", err);
+                            elwt.exit();
+                            return;
+                        }
+                    }
+
+                    // Update internal state and request a redraw
+                    shape::draw(&mut drawing, now.elapsed().as_secs_f32());
+                    window.request_redraw();
                 }
             }
-
-            // Update internal state and request a redraw
-            shape::draw(&mut drawing, now.elapsed().as_secs_f32());
-            window.request_redraw();
+            _ => {}
         }
     });
     res.map_err(|e| Error::UserDefined(Box::new(e)))
